c99dbe34c6f4f7154ac39eeb368f6bba67e89856
andiswa-phewa <andiswa0310@gmail.com>
2026-09-17 09:14:52 +0200

    Create Login.java

---
+++ ChatAAppPart1/src/main/java/za/ac/iie/prog6112/chataapppart1/Login.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.iie.prog6112.chataapppart1;
+
+/**
+ *
+ * @author anphewa
+ */
+public class Login {
+    
+}
